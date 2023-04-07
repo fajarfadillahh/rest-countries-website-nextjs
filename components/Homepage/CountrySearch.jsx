50777792e59cf89628fdae7b28e63b5eeb276f35
fajarfadillahh <fajaragustian979@gmail.com>
2023-04-07 14:53:12 +0700

    create: CountrySearch part - Home page

---
+++ components/Homepage/CountrySearch.jsx
@@ -0,0 +1,17 @@
+import { RiSearchLine } from "react-icons/ri";
+
+export default function CountrySearch() {
+  return (
+    <form className="flex h-[56px] max-w-lg items-center rounded-md bg-white shadow-sm">
+      <div className="inline-flex h-full w-[80px] items-center justify-center text-gray-600">
+        <RiSearchLine className="text-[1.3rem]" />
+      </div>
+
+      <input
+        type="text"
+        placeholder="Search for a country..."
+        className="h-full w-full bg-transparent pr-8 font-semibold text-gray-900 outline-none placeholder:text-gray-600"
+      />
+    </form>
+  );
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,19 @@
+// import components
+import CountrySearch from "@/components/Homepage/CountrySearch";
+
 export default function Home() {
   return (
-    <div className="pt-40 text-center text-[18px] font-black text-gray-900">
-      Home page
-    </div>
+    <section className="section pt-32">
+      <div className="container grid gap-8">
+        <div className="grid gap-12 lg:grid-cols-2">
+          <CountrySearch />
+          <div>country region</div>
+        </div>
+
+        <div className="grid gap-12 justify-self-center md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          <div>country card</div>
+        </div>
+      </div>
+    </section>
   );
 }
