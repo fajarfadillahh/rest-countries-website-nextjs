b80beeeeca1160f1aceb58bf15f385e0a7c3034f
fajarfadillahh <fajaragustian979@gmail.com>
2023-04-07 15:01:01 +0700

    modified: fixed Footer components

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="footer bg-white">
-      <div className="container flex h-20 items-center justify-center border-t-2 border-gray-100">
+    <footer className="footer bg-gray-500">
+      <div className="container flex h-20 items-center justify-center border-t-2 border-gray-200">
         <h1 className="font-semibold text-gray-900">
           &copy; Copyrights {new Date().getFullYear()} - Develop by ❤️
         </h1>
