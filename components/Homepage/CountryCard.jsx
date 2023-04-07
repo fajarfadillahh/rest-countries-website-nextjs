447a16e7d9fb444589f8708d7a5ef9a46d4cb757
fajarfadillahh <fajaragustian979@gmail.com>
2023-04-07 15:19:43 +0700

    modified: consume all data at the CountryCard part

--- components/Homepage/CountryCard.jsx
+++ components/Homepage/CountryCard.jsx
@@ -1,9 +1,9 @@
-export default function CountryCard() {
+export default function CountryCard({ country }) {
   return (
     <div className="w-max overflow-hidden rounded-lg shadow-sm transition-all duration-300 hover:-translate-y-1 hover:shadow-md">
       <div className="overflow-hidden">
         <img
-          src="/images/flag-be.svg"
+          src={`${country.flag}`}
           alt="country flag"
           className="h-[180px] w-[280px] object-cover object-center"
         />
@@ -11,14 +11,14 @@ export default function CountryCard() {
 
       <div className="max-w-[280px] overflow-hidden bg-white p-8">
         <h3 className="section-title truncate pb-4 text-[18px]">
-          Country name
+          {country.name}
         </h3>
 
         <ul className="flex flex-col gap-1 text-[14px]">
           {[
-            ["Population:", "1,234,567"],
-            ["Region:", "region"],
-            ["Capital:", "capital"],
+            ["Population:", `${country.population.toLocaleString()}`],
+            ["Region:", `${country.region}`],
+            ["Capital:", `${country.capital}`],
           ].map(([name, value]) => {
             return (
               <li key={name} className="flex items-center gap-1">
