ff850b48aabce27d21ef2c5ec0899ed1377e4b4e
fajarfadillahh <fajaragustian979@gmail.com>
2023-04-07 15:16:10 +0700

    modified: get all country data from api

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,7 @@ import CountryRegion from "@/components/Homepage/CountryRegion";
 import CountrySearch from "@/components/Homepage/CountrySearch";
 import CountryCard from "@/components/Homepage/CountryCard";
 
-export default function Home() {
+export default function Home({ dataCountries }) {
   return (
     <section className="section pt-32">
       <div className="container grid gap-8">
@@ -13,9 +13,22 @@ export default function Home() {
         </div>
 
         <div className="grid gap-12 justify-self-center md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          <CountryCard />
+          {dataCountries?.map((country) => {
+            return <CountryCard key={country.numericCode} country={country} />;
+          })}
         </div>
       </div>
     </section>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch("https://restcountries.com/v2/all");
+  const data = await res.json();
+
+  return {
+    props: {
+      dataCountries: data,
+    },
+  };
 }
